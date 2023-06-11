558d40bbb064c9810c408bdce6422cf322703115
Gokhan Kurt <krtgkn@gmail.com>
2023-06-12 00:34:30 +0300

    add rem-em tests

---
+++ src/tests/rem-em.test.ts
@@ -0,0 +1,54 @@
+const { testTransform } = require('./utils');
+
+const baseInput = `
+.demo {
+  font-size: rem(16px);
+  height: em(40px);
+}
+`;
+
+const mediaInput = `
+@media (min-width: em(320px)) {
+  font-size: rem(32px);
+}
+`;
+
+const remEmInsideClassNameInput = `
+.button-rem {
+  color: red;
+}
+.button-em {
+  color: red;
+}
+`;
+
+const remEmInsideFunctionInput = `
+.button1 {
+  color: theorem(32px);
+}
+.button2 {
+  color: poem(48px);
+}
+`;
+
+describe('rem-em', () => {
+  it('should work', async () => {
+    const res = await testTransform(baseInput);
+    expect(res.css).toMatchSnapshot();
+  });
+
+  it('works inside media query', async () => {
+    const res = await testTransform(mediaInput);
+    expect(res.css).toMatchSnapshot();
+  });
+
+  it('works when rem/em is inside the class name', async () => {
+    const res = await testTransform(remEmInsideClassNameInput);
+    expect(res.css).toMatchSnapshot();
+  });
+
+  it('doesnt process the function when name doesnt match exactly', async () => {
+    const res = await testTransform(remEmInsideFunctionInput);
+    expect(res.css).toMatchSnapshot();
+  });
+});
